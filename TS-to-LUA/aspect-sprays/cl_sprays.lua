159ec930688bf11683623df369c56f757a874f81
Evan <87098365+YeahMan406@users.noreply.github.com>
2022-11-26 20:37:55 +0000

    init(aspect-sprays) WIP

---
+++ TS-to-LUA/aspect-sprays/cl_sprays.lua
@@ -0,0 +1,606 @@
+local CGSprayData = {
+    isGang =  true,
+    name = 'Change Gang',
+    model =  'np_sprays_cg',
+    image = 'https://i.imgur.com/bng68KR.png',
+}
+
+local BallasSprayData = {
+    isGang = true,
+    name = 'Ballas',
+    model = 'np_sprays_ballas',
+    image = 'https://i.imgur.com/kZBWTxt.png',
+}
+
+local GSFSprayData = {
+    isGang = true,
+    name = 'Grove Street Families',
+    model = 'np_sprays_gsf',
+    image = 'https://i.imgur.com/1ntr5Qf.png',
+}
+
+local KingzSprayData = {
+    isGang = false,
+    name = 'Kingz',
+    model = 'np_sprays_kingz',
+    image = 'https://i.imgur.com/tDKhSib.png',
+}
+
+local MDMSprayData = {
+    isGang = true,
+    name = 'Mandem',
+    model = 'np_sprays_mandem',
+    image = 'https://i.imgur.com/puUouqd.png',
+}
+
+local VagosSprayData = {
+    isGang = true,
+    name = 'Vagos',
+    model = 'np_sprays_vagos',
+    image = 'https://i.imgur.com/efGwiE9.png',
+}
+
+local BBMCSprayData = {
+    isGang = true,
+    name = 'Bondi Boys MC',
+    model = 'np_sprays_bbmc',
+    image = 'https://i.imgur.com/dfPPcOX.png',
+}
+
+local BSKSprayData = {
+    isGang = true,
+    name = 'Brouge Street Kingz',
+    model = 'np_sprays_bsk',
+    image = 'https://i.imgur.com/SFerhzb.png',
+}
+
+local HOASprayData = {
+    isGang = true,
+    name = 'Hoa',
+    model = 'np_sprays_hoa',
+    image = 'https://i.imgur.com/7yQjQgr.png',
+}
+
+local NBCSprayData = {
+    isGang = true,
+    name = 'Nbc',
+    model = 'np_sprays_nbc',
+    image = 'https://i.imgur.com/AcCdobk.png',
+}
+
+local SeasideSprayData = {
+    isGang = true,
+    name = 'Seaside',
+    model = 'np_sprays_seaside',
+    image = 'https://i.imgur.com/pXu5TAO.png',
+}
+
+local AngelsSprayData = {
+    isGang = true,
+    name = 'Angels',
+    model = 'np_sprays_angels',
+    image = 'https://i.imgur.com/4YHjoOR.png',
+}
+
+local LostMCSprayData = {
+    isGang = true,
+    name = 'Lost MC',
+    model = 'np_sprays_lost',
+    image = 'https://i.imgur.com/Z8aFf9Q.png',
+}
+
+local HydraSprayData = {
+    isGang = true,
+    name = 'Hydra',
+    model = 'np_sprays_hydra',
+    image = 'https://i.imgur.com/Gfrrw1B.png',
+}
+
+local StreetTeamSprayData = {
+    isGang = false,
+    name = 'Street Team',
+    model = 'np_sprays_st',
+    servers = ['in', 'wl', 'br', 'spa'],
+    image = 'https://i.imgur.com/UUidVhi.png',
+}
+
+local TheGuildSprayData = {
+    isGang = true,
+    name = 'The Guild',
+    model = 'np_sprays_guild',
+    image = 'https://i.imgur.com/AGqR9z6.png',
+}
+
+local RonSprayData = {
+    isGang = false,
+    name = 'Ron',
+    model = 'np_sprays_ron',
+    image = 'https://i.imgur.com/m4lDqsh.png',
+}
+
+local MichealSimoneSprayData = {
+    isGang = false,
+    shopHidden = true,
+    name = 'Michael Simone',
+    model = 'np_sprays_michael',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local DiceGodSprayData = {
+    isGang = false,
+    shopHidden = true,
+    name = 'Dice God',
+    model = 'np_sprays_dicegod',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local GGSprayData = {
+    isGang = true,
+    name = 'Gang Gang',
+    model = 'np_sprays_gg',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local SCUSprayData = {
+    isGang = false,
+    name = 'SCU',
+    model = 'np_sprays_scu',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local MayhemSprayData = {
+    isGang = false,
+    name = 'Mayhem',
+    model = 'np_sprays_mayhem',
+    servers = ['in', 'wl', 'br', 'spa'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local BCFSprayData = {
+    isGang = false,
+    name = 'BCF',
+    model = 'np_sprays_bcf',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local RustSprayData = {
+    isGang = true,
+    name = 'Rust',
+    model = 'np_sprays_rust',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local PitchersSprayData = {
+    isGang = false,
+    name = 'Pitchers',
+    model = 'np_sprays_pitchers',
+    image = 'https://i.imgur.com/LDnpBbk.png',
+}
+
+local MarabuntaSprayData = {
+    isGang = true,
+    name = 'Marabunta',
+    model = 'np_sprays_marabunta',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local YokaiSprayData = {
+    isGang = false,
+    name = 'Yokai',
+    model = 'np_sprays_yokai',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local RatBoiSprayData = {
+    isGang = false,
+    name = 'Ratboi',
+    model = 'np_sprays_ratboy',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local QueerSprayData = {
+    isGang = false,
+    name = 'Pride',
+    model = 'np_sprays_pride',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local RoyalMafiaSprayData = {
+    isGang = false,
+    name = 'Royal Mafia',
+    model = 'np_sprays_rm',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local TheHiddenSprayData = {
+    isGang = false,
+    name = 'The Hidden',
+    model = 'np_sprays_hidden',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local TheSaintsSprayData = {
+    isGang = false,
+    name = 'SAINTS',
+    model = 'np_sprays_saints',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local BCGSprayData = {
+    isGang = true,
+    name = 'Bowlcut Gang',
+    model = 'np_sprays_public_bowlcutgang',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local DirtyBoisSprayData = {
+    isGang = true,
+    name = 'Dirty Bois',
+    model = 'np_sprays_public_dirtybois',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local EastsideKingzSprayData = {
+    isGang = true,
+    name = 'Eastside Kingz',
+    model = 'np_sprays_public_eastsidekingz',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local InnerCircleSprayData = {
+    isGang = true,
+    name = 'Inner Circle',
+    model = 'np_sprays_public_innercircle',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local LaFamilliaSprayData = {
+    isGang = true,
+    name = 'La Familia',
+    model = 'np_sprays_public_lafamilia',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local NrthsideLegionSprayData = {
+    isGang = true,
+    name = 'Nrthside Legion',
+    model = 'np_sprays_public_northsidelegion',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local RoyalBlackSprayData = {
+    isGang = true,
+    name = 'Royal Black',
+    model = 'np_sprays_public_royalblack',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local SaintsSprayData = {
+    isGang = true,
+    name = 'Saints',
+    model = 'np_sprays_public_saints',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local SinisterSeoulsSprayData = {
+    isGang = true,
+    name = 'Sinister Souls',
+    model = 'np_sprays_public_sinistersoulsmc',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local SkullGangSprayData = {
+    isGang = true,
+    name = 'Skullgang',
+    model = 'np_sprays_public_skullgang',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local SpanonisSprayData = {
+    isGang = true,
+    name = 'Spanonis',
+    model = 'np_sprays_public_spanonis',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local TheContinentalFamilySprayData = {
+    isGang = true,
+    name = 'The Contiental Family',
+    model = 'np_sprays_public_thecontientalfamily',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local TheLegionSprayData = {
+    isGang = true,
+    name = 'The Legion',
+    model = 'np_sprays_public_thelegion',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local TheRoadMenSprayData = {
+    isGang = true,
+    name = 'The Road Men',
+    model = 'np_sprays_public_theroadmen',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local Yokai2SprayData = {
+    isGang = true,
+    name = 'Yokai',
+    model = 'np_sprays_public_yokai',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local BlackdogsSprayData = {
+    isGang = true,
+    name = 'Blackdogs',
+    model = 'np_sprays_public_blackdogs',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local AztecasSprayData = {
+    isGang = true,
+    name = 'Aztecas',
+    model = 'np_sprays_public_aztecas',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local BubbleBoisSprayData = {
+    isGang = true,
+    name = 'Bubble Bois',
+    model = 'np_sprays_public_bubblebois',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local MayhemPublicSprayData = {
+    isGang = true,
+    name = 'Mayhem',
+    model = 'np_sprays_mayhem',
+    servers = ['pa', 'pg'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local RoyalMafiaPublicSprayData = {
+    isGang = true,
+    name = 'Royal Mafia',
+    model = 'np_sprays_rm_spain',
+    servers = ['spa'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local SpanishStreetTeamSprayData = {
+    isGang = true,
+    name = 'Street Team',
+    model = 'np_sprays_st_spain',
+    servers = ['spa'],
+    image = 'https://i.imgur.com/UUidVhi.png',
+}
+
+local SpanishYokaiSprayData = {
+    isGang = true,
+    name = 'Yokai',
+    model = 'np_sprays_yokai_spain',
+    servers = ['spa'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local GambinoSpanishSprayData = {
+    isGang = true,
+    name = 'Gambino',
+    model = 'np_sprays_gambino_spain',
+    servers = ['spa'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local ZhaoShiSprayData = {
+    isGang = true,
+    name = 'Zhao Shi',
+    model = 'np_sprays_ZhaoShi',
+    servers = ['spa'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local PlaceHolderSprayData1 = {
+    isGang = false,
+    name = 'Placeholder 01',
+    model = 'np_sprays_ph_01',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local PlaceHolderSprayData2 = {
+    isGang = false,
+    name = 'Placeholder 02',
+    model = 'np_sprays_ph_02',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local PlaceHolderSprayData3 = {
+    isGang = false,
+    name = 'Placeholder 03',
+    model = 'np_sprays_ph_03',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local PlaceHolderSprayData4 = {
+    isGang = false,
+    name = 'Placeholder 04',
+    model = 'np_sprays_ph_04',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local PlaceHolderSprayData5 = {
+    isGang = false,
+    name = 'Placeholder 05',
+    model = 'np_sprays_ph_05',
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local SpanishMayhemSprayData = {
+    isGang = true,
+    name = 'Mayhem',
+    model = 'np_sprays_mayhem',
+    servers = ['spa'],
+    image = 'https://i.imgur.com/voYVfyE.png',
+}
+
+local getAllSprayz = {
+    cg = CGSprayData,
+    ballas = BallasSprayData,
+    gsf = GSFSprayData,
+    kingz = KingzSprayData,
+    mandem = MDMSprayData,
+    vagos = VagosSprayData,
+    bbmc = BBMCSprayData,
+    bsk = BSKSprayData,
+    hoa = HOASprayData,
+    nbc = NBCSprayData,
+    seaside = SeasideSprayData,
+    angels = AngelsSprayData,
+    lostmc = LostMCSprayData,
+    hydra = HydraSprayData,
+    st = StreetTeamSprayData,
+    guild = TheGuildSprayData,
+    ron = RonSprayData,
+    michael = MichealSimoneSprayData,
+    dicegod = DiceGodSprayData,
+    gg = GGSprayData,
+    scu = SCUSprayData,
+    mayhem = MayhemSprayData,
+    bcf = BCFSprayData,
+    rust = RustSprayData,
+    pitchers = PitchersSprayData,
+    marabunta = MarabuntaSprayData,
+    yokai = YokaiSprayData,
+    ratboi = RatBoiSprayData,
+    pride = QueerSprayData,
+    rm = RoyalMafiaSprayData,
+    hidden = TheHiddenSprayData,
+    Saints = TheSaintsSprayData,
+    public_bowlcutgang = BCGSprayData,
+    public_dirtybois = DirtyBoisSprayData,
+    public_eastsidekingz = EastsideKingzSprayData,
+    public_innercircle = InnerCircleSprayData,
+    public_lafamilia = LaFamilliaSprayData,
+    public_northsidelegion = NrthsideLegionSprayData,
+    public_royalblack = RoyalBlackSprayData,
+    public_saints = SaintsSprayData,
+    public_sinistersoulsmc = SinisterSeoulsSprayData,
+    public_skullgang = SkullGangSprayData,
+    public_spanonis = SpanonisSprayData,
+    public_thecontientalfamily = TheContinentalFamilySprayData,
+    public_thelegion = TheLegionSprayData,
+    public_theroadmen = TheRoadMenSprayData,
+    public_yokai = Yokai2SprayData,
+    public_blackdogs = BlackdogsSprayData,
+    public_aztecas = AztecasSprayData,
+    public_bubblebois = BubbleBoisSprayData,
+    public_mayhem = MayhemPublicSprayData,
+    spain_rm = RoyalMafiaPublicSprayData,
+    spain_st = SpanishStreetTeamSprayData,
+    spain_yokai = SpanishYokaiSprayData,
+    spain_gambino = GambinoSpanishSprayData,
+    spain_zhaoshi = ZhaoShiSprayData,
+    placeholder_01 = PlaceHolderSprayData1,
+    placeholder_02 = PlaceHolderSprayData2,
+    placeholder_03 = PlaceHolderSprayData3,
+    placeholder_04 = PlaceHolderSprayData4,
+    placeholder_05 = PlaceHolderSprayData5,
+    spain_mayhem = SpanishMayhemSprayData,
+}
+
+AddEventHandler('np-inventory:itemUsed', function(itemID, itemData, spray)
+    if itemID == 'spraycan' then
+        print('This is spray data | itemData: '..itemData)
+        print('This is the spray: spray: '..spray)
+        local itemInformation = JSON.parse(itemData)
+        AspectSpraysAttemptPlaceSpray(itemInformation.model, spray, itemInformation.gangId)
+    end
+end)
+
+billboardModel = {
+    GetHashKey('np_billboard_01'),
+    GetHashKey('np_billboard_02'),
+    GetHashKey('np_billboard_03'),
+}
+
+function AspectSpraysAttemptPlaceSpray(graffitiPlacementCord, itemData, memberCount)
+    local itemInformation = exports['np-inventory']:GetItemInfo(itemData)
+    if itemInformation <= 0 then
+      return
+    end
+    exports['np-objects'].PlaceObject(
+      GetHashKey(graffitiPlacementCord),
+      {
+        collision: false,
+        groundSnap: false,
+        adjustZ: false,
+        distance: 3,
+        alignToSurface: true,
+        surfaceOffset: 0.025,
+        afterRender: (sprayModel, sprayModelRenderAmount, AfterRenderZZZ) => {
+            sprayEntityVector = GetEntityForwardVector(sprayModel),
+            [SprayZzZz, SprayZzZzZz, SprayZzZzZzZzZz] = GetEntityCoords(
+              sprayModel,
+              false
+            )
+          sprayModelRenderAmount and
+          DrawMarker(23, SprayZzZz - sprayEntityVector[0] * 0.03, SprayZzZzZz - sprayEntityVector[1] * 0.03, SprayZzZzZzZzZz - sprayEntityVector[2] * 0.03, ...sprayEntityVector, 90, 0, 0, 1, 1, 1, AfterRenderZZZ ? 0 : 255, AfterRenderZZZ ? 255 : 0, 0, 50, false, false, 2, false, null, null, false)
+          SetEntityAlpha(sprayModel, sprayModelRenderAmount ? 200 : 0, false)
+        },
+      },
+      (sprayCoordinates, _0x504ecd, thisSprayModel, thisSprayMhmModel) => {
+        const sprayEntityType = GetEntityType(thisSprayMhmModel)
+        let sprayBeingPlaced = false
+        if (sprayEntityType ~= 0) then
+          if sprayEntityType === 3 and billboardModel.includes(GetEntityModel(thisSprayMhmModel)) then
+            sprayBeingPlaced = true
+          else 
+            return false
+          end
+        end
+        if exports['np-doors'].GetCurrentDoor() ~= undefined then
+          return false
+        end
+        sprayBeingPlacedModel = GetEntityModel(thisSprayModel),
+          [[sprayBeingPlacedRotation, , sprayBeingPlacedRotationMhm2], [shrugIdfk, shrugIdfk2, shrugIdfk3]] = GetModelDimensions(sprayBeingPlacedModel),
+            sprayForwardVector = GetEntityForwardVector(thisSprayModel).map((_0x428787) => _0x428787 * 0.2)
+        return (
+          [
+            GetOffsetFromEntityInWorldCoords(thisSprayModel, sprayBeingPlacedRotation * 0.8, shrugIdfk2, sprayBeingPlacedRotationMhm2 * 0.8),
+            GetOffsetFromEntityInWorldCoords(thisSprayModel, shrugIdfk * 0.8, shrugIdfk2, sprayBeingPlacedRotationMhm2 * 0.8),
+            GetOffsetFromEntityInWorldCoords(thisSprayModel, sprayBeingPlacedRotation * 0.8, shrugIdfk2, shrugIdfk3 * 0.8),
+            GetOffsetFromEntityInWorldCoords(thisSprayModel, shrugIdfk * 0.8, shrugIdfk2, shrugIdfk3 * 0.8),
+          ].every((sproy) => {
+            const startSprayFadeIn = StartShapeTestCapsule(...sproy, sproy[0] + sprayForwardVector[0],
+              sproy[1] + sprayForwardVector[1],
+              sproy[2] + sprayForwardVector[2],
+              0.02,
+              -1,
+              thisSprayModel,
+              7
+            ),
+              [, _0x327ff7, , , _0x540500] = GetShapeTestResult(startSprayFadeIn),
+              _0x48ae5a = GetEntityType(_0x540500)
+            return _0x327ff7 && (_0x48ae5a === 0 || sprayBeingPlaced)
+          }) && !_0x189142([sprayCoordinates.x, sprayCoordinates.y, sprayCoordinates.z])
+        )
+      }
+end)
